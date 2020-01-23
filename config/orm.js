2feda98af7410b9d617c025990b458bc1fbd6a03
Kfriedman <Koryf91@gmail.com>
2020-01-22 20:31:49 -0800

    orm file

--- config/orm.js
+++ config/orm.js
@@ -1,17 +1,59 @@
 var connection = require("./connection.js");
 
 var orm = {
-selectAll: function(tableName){
-    var queryString = "SELECT * FROM " +tableName;
-    connection.query(queryString, function(err, res){
-        console.log(res);
-    })
-},
-insertOne: function(tableName, columnNmaes, columnValues, callback){
-var querySting = "INSERT INTO" + tableName + "(" + columnNmaes +")" +"VALUES (" +columnValues +")";
-console.log(querySting)
+    selectAll: function (tableName) {
+        var queryString = "SELECT * FROM " + tableName;
+        connection.query(queryString, function (err, res) {
 
-}
+            if (err) {
+                throw err;
+            }
+            cb(result);
+
+            console.log(res);
+        })
+    },
+    insertOne: function (tableName, columnNmaes, columnValues, callback) {
+        var querySting = "INSERT INTO" + tableName + "NAMES: (" + columnNmaes + ")" + "VALUES (" + columnValues + ")";
+        console.log(querySting)
+        connection.query(queryString, vals, function (err, result) {
+            if (err) {
+                throw err;
+            }
+
+            cb(result);
+        });
+    },
+    update: function(table, objColVals, condition, cb) {
+        var queryString = "UPDATE " + table;
+    
+        queryString += " SET ";
+        queryString += objToSql(objColVals);
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        console.log(queryString);
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      },
+      delete: function(table, condition, cb) {
+        var queryString = "DELETE FROM " + table;
+        queryString += " WHERE ";
+        queryString += condition;
+    
+        connection.query(queryString, function(err, result) {
+          if (err) {
+            throw err;
+          }
+    
+          cb(result);
+        });
+      }
 }
 
 module.exports = orm;
